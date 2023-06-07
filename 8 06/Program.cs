8078cc25152c0385c422ac71a0096fad66444dc7
Luka Semenikhin <117368298+lukasemenikhin@users.noreply.github.com>
2023-06-07 17:56:50 +0400

    Add files via upload

---
+++ 8 06/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("shemoitanet mteli ricxvi:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[number];
+for (int i = 0; i < number; i++)
+{
+    array[i] = i;
+}
+
+
